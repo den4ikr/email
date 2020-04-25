d7a56b1a372a5a31a38e3c636b8568c49322b014
den4ikr <55842394+den4ikr@users.noreply.github.com>
2020-04-25 14:28:39 +0300

    commit

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
 if (isset ($_POST['contactFF'])) {
   $to = "danirom0401@gmail.com";
-  //$from = "support@tpverstak.ru";
+  $from = "https://den4ikr.github.io/email/";
   $subject = "Заполнена контактная форма на сайте ".$_SERVER['HTTP_REFERER'];
   $message = "Имя пользователя: ".$_POST['nameFF']."\nEmail пользователя ".$_POST['contactFF']."\nТелефон пользователя ".$_POST['telFF']."\nСообщение: ".$_POST['projectFF']."\n\nАдрес сайта: ".$_SERVER['HTTP_REFERER'];
  
